a9e4d81f73322b7d26fe9e5dfddff80017aefa91
Sagar Kathariya <sagarkathariya@gmail.com>
2021-11-19 14:39:04 +0545

    first commit of otp generator

---
+++ src/otp_generator/otp_generator.java
@@ -0,0 +1,17 @@
+package otp_generator;
+
+import java.util.Random;
+
+public class otp_generator {
+	public static void main(String[] args) {
+		String num = "0123456789";
+		int len = 6;
+		char[] otp = new char[len];
+		Random r = new Random();
+		for (int i = 0; i < len; i++) {
+			otp[i] = num.charAt(r.nextInt(num.length()));
+
+		}
+		System.out.println(" Your OTP is :- " + new String(otp));
+	}
+}
